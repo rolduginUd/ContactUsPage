a1671904190ec6a680b26454f5270ec5cc5103b5
Eugene <roldugin.ud@gmail.com>
2022-12-05 23:10:29 +0200

    custom select

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+document.querySelector('.select-wrapper').addEventListener('click', function() {
+    this.querySelector('.select').classList.toggle('open');
+})
+
+for (const option of document.querySelectorAll(".custom-option")) {
+    option.addEventListener('click', function() {
+        if (!this.classList.contains('selected')) {
+            this.parentNode.querySelector('.custom-option.selected').classList.remove('selected');
+            this.classList.add('selected');
+            this.closest('.select').querySelector('.select__trigger span').textContent = this.textContent;
+        }
+    })
+}
+
+window.addEventListener('click', event => {
+    console.log(event.target);
+})
